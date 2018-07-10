b196ea87728c15ec2baab17aa9339b42518e2962
KimVesperia <kim.vphung@outlook.com>
2018-07-11 01:07:51 +0200

    versie 6, editable imageview en styling

--- app/src/main/java/kimv/loginregister/ProfileActivity.java
+++ app/src/main/java/kimv/loginregister/ProfileActivity.java
@@ -1,5 +1,7 @@
 package kimv.loginregister;
 
+import android.Manifest;
+import android.app.VoiceInteractor;
 import android.content.Intent;
 import android.database.Cursor;
 import android.graphics.Bitmap;
@@ -7,6 +9,7 @@ import android.graphics.BitmapFactory;
 import android.net.Uri;
 import android.os.Build;
 import android.provider.MediaStore;
+import android.provider.Settings;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -15,10 +18,24 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.FirebaseDatabase;
+
+import org.json.JSONObject;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
 
 public class ProfileActivity extends AppCompatActivity implements View.OnClickListener {
 
@@ -26,6 +43,8 @@ public class ProfileActivity extends AppCompatActivity implements View.OnClickLi
     private String nam2;
     private TextView textViewUserEmail;
     private Button buttonLogout;
+
+    private static int RESULT_LOAD_IMAGE = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -41,10 +60,23 @@ public class ProfileActivity extends AppCompatActivity implements View.OnClickLi
 
         FirebaseUser user = firebaseAuth.getCurrentUser();
 
-        textViewUserEmail = (TextView) findViewById(R.id.textViewUserEmail);
+//        textViewUserEmail = (TextView) findViewById(R.id.textViewUserEmail);
+
+        Button buttonLoadImage = (Button) findViewById(R.id.selectImage);
+        buttonLoadImage.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View V) {
+
+                Intent intent = new Intent();
+                intent.setType("image/*");
+                intent.setAction(Intent.ACTION_GET_CONTENT);
+                startActivityForResult(Intent.createChooser(intent, "Select Picture"), RESULT_LOAD_IMAGE);
+            }
+        });
 
         //Welkoms text met username
-        textViewUserEmail.setText("Welkom, " + user.getDisplayName());
+//        textViewUserEmail.setText(user.getDisplayName());
 
         Button btn1 = (Button) findViewById(R.id.buttonCourseYear);
         btn1.setOnClickListener(new View.OnClickListener() {
@@ -74,6 +106,53 @@ public class ProfileActivity extends AppCompatActivity implements View.OnClickLi
         buttonLogout = (Button) findViewById(R.id.buttonLogout);
 
         buttonLogout.setOnClickListener(this);
+
+        String URL = "https://swapi.co/api/starships/9/";
+
+        RequestQueue requestQueue = Volley.newRequestQueue(this);
+
+        JsonObjectRequest objectRequest = new JsonObjectRequest(
+
+                Request.Method.GET,
+                URL,
+                null,
+                new Response.Listener<JSONObject>() {
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        Log.e("Rest Response", response.toString());
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        Log.e("Rest Response", error.toString());
+                    }
+                }
+        );
+
+        requestQueue.add(objectRequest);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        ImageView imageView = (ImageView) findViewById(R.id.imageView);
+
+        if (requestCode == RESULT_LOAD_IMAGE && resultCode == RESULT_OK && null != data) {
+            try{
+                final Uri uriImage = data.getData();
+                final InputStream inputStream = getContentResolver().openInputStream(uriImage);
+                final Bitmap imageMap = BitmapFactory.decodeStream(inputStream);
+                imageView.setImageBitmap(imageMap);
+
+//                startActivity(new Intent(ProfileActivity.this, ProfileActivity.class));
+            }
+            catch (FileNotFoundException e) {
+                e.printStackTrace();
+                Toast.makeText(ProfileActivity.this, "Image was not found", Toast.LENGTH_SHORT).show();
+            }
+
+        }
     }
 
 
